546ebd24b7cde394c319b78840a8a3e29a8dcc9f
hassaan1shakil <hassaan.shakil1@gmail.com>
2024-10-03 17:58:46 +0500

    User Signup & User Login w/o Token Completed

--- backend/WardrobeWizard/WardrobeWizard/settings.py
+++ backend/WardrobeWizard/WardrobeWizard/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "api.apps.ApiConfig",
     "corsheaders",
     'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -66,7 +67,7 @@ CORS_ALLOWED_ORIGINS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
