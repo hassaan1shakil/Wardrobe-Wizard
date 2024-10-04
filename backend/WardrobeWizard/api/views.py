0770c8e1faefd96d255d0ff6fd49b87a2276b859
hassaan1shakil <hassaan.shakil1@gmail.com>
2024-10-04 17:20:48 +0500

    Token Generation Completed

--- backend/WardrobeWizard/api/views.py
+++ backend/WardrobeWizard/api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework import generics, status
 from rest_framework.response import Response
 from .serializers import UserSignupSerializer, LoginSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
 
 # Create your views here.
 
@@ -23,12 +25,26 @@ class LoginView(generics.CreateAPIView):
     
     serializer_class = LoginSerializer
     
+    # permission_classes = [IsAuthenticated]    --> Setting this does not allow any unauthorized user to even access the login endpoint which is not what we want. That is why, it will set to [AllowAny] or not set at all.
+    
     def post(self, request, *args, **kwargs):
         
         serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
         
-        token = 99999 #.gettoken()
+        if serializer.is_valid():
+            
+            user = serializer.validated_data['user']
+
+            #Generate Refresh & Access Tokens
+            
+            refresh_token = RefreshToken.for_user(user)
+            access_token = str(refresh_token.access_token)
+            
+            return Response(
+                {"access_token": access_token,
+                 "refresh_token": str(refresh_token)
+                },
+                status=status.HTTP_200_OK
+            )
         
-        return Response({"message": "User logged in successfully", "Bearer": token, "username": user.username}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
